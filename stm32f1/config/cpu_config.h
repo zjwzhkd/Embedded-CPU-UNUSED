62e110f6cb5ea69f907bdf29fef43f67dc6a765e
Keda Huang <huangkeda88@163.com>
2016-08-26 18:36:26 +0800

    修改cpu_config.h
    - 添加内存字节对齐(CPU_BYTE_ALIGNMENT)
    - 修改注释

--- stm32f1/config/cpu_config.h
+++ stm32f1/config/cpu_config.h
@@ -11,14 +11,15 @@
 #define __CPU_CONFIG_H
 
 /* CPU参数配置 ---------------------------------------------------------------*/
-#define CPU_FREQ_HZ         ( (uint32_t) 72000000 )  /* CPU运行频率(Hz)       */
-#define CPU_TIMER_HZ        ( (uint32_t) 72000000 )  /* CPU节拍定时器频率(Hz) */
-#define CPU_TICK_HZ         ( (uint32_t) 1000 )      /* CPU节拍频率(Hz)       */
+#define CPU_FREQ_HZ         ( (uint32_t) 72000000 ) /* CPU运行频率(Hz)        */
+#define CPU_TIMER_HZ        ( (uint32_t) 72000000 ) /* CPU节拍定时器频率(Hz)  */
+#define CPU_TICK_HZ         ( (uint32_t) 1000 )     /* CPU节拍频率(Hz)        */
+#define CPU_BYTE_ALIGNMENT  ( 8 )                   /* CPU内存字节对齐        */
 
 /* CPU调试配置 ---------------------------------------------------------------*/
-#define CPU_ASSERT_EN       ( 1 )                    /* 调试断言功能使能      */
-#define CPU_COVERAGE_EN     ( 1 )                    /* 调试代码覆盖功能使能  */
-#define CPU_PRINTF_EN       ( 1 )                    /* 调试输出功能使能      */
+#define CPU_ASSERT_EN       ( 1 )                   /* 调试断言功能使能       */
+#define CPU_COVERAGE_EN     ( 1 )                   /* 调试代码覆盖功能使能   */
+#define CPU_PRINTF_EN       ( 1 )                   /* 调试输出功能使能       */
 
 /* CPU宏定义 -----------------------------------------------------------------*/
 #define CPU_TICK_PERIOD_IS_1MS
